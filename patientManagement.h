0a6696054e038937d948ac190e4cb3353153f05b
haodarshika <darshikaoshani@gmail.com>
2026-09-20 13:57:53 +0530

    Add patient  management header file

---
+++ patientManagement.h
@@ -0,0 +1,19 @@
+#ifndef PATIENT_MANAGEMENT_H
+
+#define PATIENT_MANAGEMENT_H
+
+#define MAX_PATIENTS 100
+
+extern char patientNames[MAX_PATIENTS][50];
+extern int patientAges[MAX_PATIENTS];
+extern int urgencyLevels[MAX_PATIENTS];
+extern int specialtyIDs[MAX_PATIENTS];
+extern int wardAdmissions[MAX_PATIENTS];
+extern int wardIDs[MAX_PATIENTS];
+extern int days Admitted[MAX_PATIENTS];
+extern int totalPatients;
+
+
+void  registerPatient();
+void  displayPatientQueue()
+#endif
