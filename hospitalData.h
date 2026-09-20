5ce9a4bd6948d7f2c0e1ffac666ec7128dc2bbda
haodarshika <darshikaoshani@gmail.com>
2026-09-20 19:16:14 +0530

    Finalize hospital management system implementation and reports

--- hospitalData.h
+++ hospitalData.h
@@ -9,6 +9,25 @@
 
 extern const char specialtyNames[TOTAL_SPECIALTIES][30];
 extern const double baseConsultationFees[TOTAL_SPECIALTIES];
-extern const int  consultationTimes[TOTAL_SPECIALTIES];
+extern const int consultationTimes[TOTAL_SPECIALTIES];
+extern const int dailyPatientCaps[TOTAL_SPECIALTIES];
+
+extern const char wardNames[TOTAL_WARDS][30];
+extern const double dailyBedRates[TOTAL_WARDS];
+extern const int totalBedCapacities[TOTAL_WARDS];
+
+extern int bedOccupancy[TOTAL_WARDS][MAX_BED_CAPACITY];
+
+extern char patientNames[MAX_PATIENTS][50];
+extern int patientAges[MAX_PATIENTS];
+extern int urgencyLevels[MAX_PATIENTS];
+extern int specialtyIDs[MAX_PATIENTS];
+extern int wardAdmissions[MAX_PATIENTS];
+extern int wardIDs[MAX_PATIENTS];
+extern int bedNumbers[MAX_PATIENTS];
+extern int daysAdmitted[MAX_PATIENTS];
+extern double finalPayableAmounts[MAX_PATIENTS];
+extern int totalPatients;
+extern int specialtyQueueCounts[TOTAL_SPECIALTIES];
 
 #endif
