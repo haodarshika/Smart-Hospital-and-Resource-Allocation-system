a83b8629c00d993adeaacd10a16f3add75b73ee8
haodarshika <darshikaoshani@gmail.com>
2026-09-20 13:19:28 +0530

    Add hospital data implementation for specialties and fees

---
+++ hospitalData.c
@@ -0,0 +1,6 @@
+#include "hospitalData.h"
+const char specialtyNames[TOTAL_SPECIALTIES][30]={"Cardiology"," Neurology","Pediatrics","Orthopedics"};
+const double baseConsultationFees[TOTAL_SPECIALTIES]={1500.00,2000.00,1200.00,1300.00};
+const int consultationTimes[TOTAL_SPECIALTIES]={20,30,15,20};
+
+
